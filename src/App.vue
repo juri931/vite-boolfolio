21d01c96f59b380c53f6271745e0676d5d2b3e43
juri931 <impa.mat@hotmail.it>
2024-05-31 16:00:40 +0200

    "Update App.vue template and add styles to main.scss"

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@ export default {};
 </script>
 
 <template>
-  <div>Contenuto</div>
+  <div class="main-wrapper">
+    <h1>Boolfolio Home</h1>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
